5d6a9394c1af1d88bb4987691c33e563a5e37386
RAFAELULCHAK <rafael.ulchak@escola.pr.gov.br>
2026-09-21 08:11:22 -0300

    quakekoiza

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const CaixaPrincipal = document.quarySelector(".caixa-principal");
+const CaixaPerguntas = document.quarySelector(".Caixa-perguntas");
+const CaixaAlternativas = document.quarySelector(".caixa-alternativas");
+const CaixaResultados = document.quarySelector(".caixa-resultados");
+const textoResultados = document.quarySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+     enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+     enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+         enunciado: "pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+]
